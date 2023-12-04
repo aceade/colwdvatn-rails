4b6f5c79afbf2e882d9bf3926863f8e786fdc1b0
aceade@github.com <philiprowlands90@gmail.com>
2023-12-04 09:21:40 +0000

    #3 adding navbar, fixing route

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,9 +1,10 @@
 import "./Home.css";
+import Navbar from "./navbar/Navbar";
 import { ArrivalAndDeparture, inboundStations, outboundStations, StationEntry } from "./stations";
 
 export default function Home() {
     
-    const buildDateEntry = (date: ArrivalAndDeparture) => {
+    const buildDateEntry = (date: ArrivalAndDeparture, index: number) => {
         let str = "";
         if (date.arrival) {
             str += `Arr: ${date.arrival}\n`;
@@ -11,11 +12,12 @@ export default function Home() {
         if (date.departure) {
             str += `Dep: ${date.departure}`;
         }
-        return <td>{str}</td>;
+        return <td key={index}>{str}</td>;
     }
 
     return (
         <>
+            <Navbar></Navbar>
             <main>
                 <h1>Colwdvatn & Kerbalstead Rails</h1>
                 <p>Your gateway to The Sydfjords</p>
@@ -25,23 +27,27 @@ export default function Home() {
                         <h2>Live Trains (Inbound)</h2>
 
                         <table>
-                            <tr>
-                                <th>Station</th>
-                            </tr>
+                            <thead>
+                                <tr>
+                                    <th>Station</th>
+                                </tr>
+                            </thead>
+                            <tbody>
                             {
-                                inboundStations.map((station: StationEntry) => {
+                                inboundStations.map((station: StationEntry, index: number) => {
                                     return (
-                                        <tr>
+                                        <tr key={index}>
                                             <td className="rowHeader">
                                                 {station.location}
                                             </td>
-                                            {station.dates.map((date) => {
-                                                return buildDateEntry(date);
+                                            {station.dates.map((date, index: number) => {
+                                                return buildDateEntry(date, index);
                                             })}
                                         </tr>
                                     );
                                 })
                             }
+                            </tbody>
                         </table>
                     </header>
                 </section>
@@ -49,23 +55,27 @@ export default function Home() {
                     <header>
                         <h2>Outbound Trains</h2>
                         <table>
-                            <tr>
-                                <th>Station</th>
-                            </tr>
+                            <thead>
+                                <tr>
+                                    <th>Station</th>
+                                </tr>
+                            </thead>
+                            <tbody>
                             {
-                                outboundStations.map((station: StationEntry) => {
+                                outboundStations.map((station: StationEntry, index: number) => {
                                     return (
-                                        <tr>
+                                        <tr key={index}>
                                             <td className="rowHeader">
                                                 {station.location}
                                             </td>
-                                            {station.dates.map((date) => {
-                                                return buildDateEntry(date);
+                                            {station.dates.map((date, index) => {
+                                                return buildDateEntry(date, index);
                                             })}
                                         </tr>
                                     );
                                 })
                             }
+                            </tbody>
                         </table>
                     </header>
                 </section>
